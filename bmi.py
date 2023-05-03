19acc8a1a2227b03a203a042027680840d48a5eb
Aidan <aidang.22@ichat.sp.edu.sg>
2023-05-03 09:38:06 +0800

    return values added to bmi

--- bmi.py
+++ bmi.py
@@ -12,9 +12,12 @@ def calculate_bmi(height, weight):
     print(BMI)
     if BMI < 18.5:
         print("Under Weight")
+        return -1
     elif BMI >= 18.5 and BMI <= 25.0:
         print("Normal Weight")
+        return 0
     elif BMI > 25.0:
         print("Over Weight")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
